688fe4e443456d59fc4d4624ff11c50ddd96918f
hadeellafi <hadeellafi9@gmail.com>
2023-04-05 07:45:08 +0000

    added 3 questions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 'use strict'
+function checkString(string){
+    if(string=="Yes")
+    arr.push("Yes");
+    else if(string=="No")
+    arr.push("No");
+    else
+    arr.push("invalid");
+}
+function printArray(tArr)//t:temporary
+{
+    for(let i=0;i<tArr.length;i++)
+    console.log(tArr[i]);
+}
 let userName=prompt("Enter your name");
 let gender=prompt("Enter your gender,The answer should be either (male or female).");
 let age=prompt("Enter your age");
@@ -27,8 +40,17 @@ if(!choice)
 
     }
 }
-// Ask the user to enter his/her gender as input. The answer should be either (male or female).
-// Ask the user to enter his/her age as input and alert the user if the age is less than or equal to zero.
-// Ask the user to confirm if he wants to skip the welcoming message.
-// Alert a welcoming message with the name of the user and the title Mr if the user is male and Ms if the user is female, and if the gender input is not correct alert the welcoming message without the title, according to the confirmation.
-// Continue working on the styling of the project.
+let arr=[];
+let input=prompt("Do you like the design of the website,answer should be (Yes/No)?");
+checkString(input);
+ input=prompt("Do you have notes of the design,answer should be (Yes/No)?");
+checkString(input);
+ input=prompt("will you come again,answer should be (Yes/No)");
+checkString(input);
+printArray(arr);
+// As a user, I would like to answer more questions, so you should add new three (Yes/No) questions.
+// If the input was empty consider the answer to be an "invalid" string.
+// Consider putting all the answers into an array.
+// Traverse the array and print the items on the console.
+// Confirm that your code is following the single responsibility rule. Each function should only do one thing.
+// All functions and variables should have a meaningful name (use camelCase).
